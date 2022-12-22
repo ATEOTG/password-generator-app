9d4ba9837e549e5a898c0cf34ba7edebe4233601
ATEOTG <bryansantos9797@gmail.com>
2022-12-21 19:37:10 -0500

    added ability to generate password and ability to copy the password to clipboard

--- script.js
+++ script.js
@@ -14,19 +14,39 @@ const barFour = document.getElementById("bar4");
 
 const strengthText = document.querySelector(".strength-desc");
 const button = document.querySelector(".generate-btn");
+const passwordValue =  document.querySelector(".password-text");
+const copyIcon = document.querySelector(".copy-icon");
+const copyText = document.querySelector(".copy-text");
 
+const lowerChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const upperChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const digits = ["0","1","2","3","4","5","6","7","8","9"];
+const symbols = ["~","!","#","$","%","^","&","*","+","(",")","-","_","[","]","|","{","}","'",":",";",`<`,`>`,",",".","?",`"`];
 const bars = [barOne, barTwo, barThree, barFour];
 let prevCount = 0;
+let password = "";
 
 sliderDisplay();
 prevCount = strengthBarDisplay();
 
+
+
+copyIcon.addEventListener("click", copied);
 slider.addEventListener("input", sliderDisplay);
 button.addEventListener("click", function (e) {
   e.preventDefault();
   if (getCount() === 0) alert("Please check at least one box!");
+  else {
+    generatePassword();
+  }
 });
 checkBoxes.forEach((box) => box.addEventListener("click", strengthBarDisplay));
+
+function copied(){
+  copyText.classList.add("active");
+  navigator.clipboard.writeText(password);
+  setTimeout(() => {copyText.classList.remove("active")},4000);
+}
 
 function sliderDisplay() {
   const valPercent = (slider.value / slider.max) * 100;
@@ -77,4 +97,24 @@ function changeBarClass(prevCount, newClass) {
     bar.classList.remove(`count${prevCount}`);
     bar.classList.add(newClass);
   }
+}
+
+function generatePassword(){
+  const options = [];
+  if(upperCaseBox.checked) options.push(upperChars);
+  if(lowerCaseBox.checked) options.push(lowerChars);
+  if(numberBox.checked) options.push(digits);
+  if(symbolBox.checked) options.push(symbols);
+
+  password = "";
+  for(let i = 0; i < slider.value; i++){
+     const selectedArr = options[Math.floor(Math.random() * options.length)];
+     const selectedValue = selectedArr[Math.floor(Math.random() * selectedArr.length)];
+     password += selectedValue;
+  }
+  
+  const passwordFinal = password.replace(/</g, "&#60;");
+  passwordValue.innerHTML = passwordFinal;
+  passwordValue.style.color = `#e6e5ea`;
+  passwordValue.style.opacity = "100%";
 }
