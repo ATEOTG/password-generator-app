cf659a65b706d2c2c31b21594207a971cf304daa
ATEOTG <bryansantos9797@gmail.com>
2022-12-21 20:56:13 -0500

    changed min/max in our range

--- script.js
+++ script.js
@@ -23,12 +23,12 @@ const upperChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 const digits = ["0","1","2","3","4","5","6","7","8","9"];
 const symbols = ["~","!","#","$","%","^","&","*","+","(",")","-","_","[","]","|","{","}","'",":",";",`<`,`>`,",",".","?",`"`];
 const bars = [barOne, barTwo, barThree, barFour];
+
 let prevCount = 0;
 let password = "";
 
 sliderDisplay();
 prevCount = strengthBarDisplay();
-
 
 
 copyIcon.addEventListener("click", copied);
@@ -49,7 +49,7 @@ function copied(){
 }
 
 function sliderDisplay() {
-  const valPercent = (slider.value / slider.max) * 100;
+  const valPercent = ((slider.value - 8) / (slider.max - 8)) * 100;
   slider.style.background = `linear-gradient(to right, #a4ffaf ${valPercent}%, #18171f ${valPercent}%)`;
   sliderText.innerHTML = slider.value;
 }
